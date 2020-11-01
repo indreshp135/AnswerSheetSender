adaffd14c65e5b1b3c60471d8c77242f73dab2f3
indreshp135 <indreshp135@gmail.com>
2020-11-01 23:04:36 +0530

    [feat] Answer paper sender
    Complete project done
    Committed with node_modules and .env to help all users.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # AnswerSheetSender
 This repo can be used to send answer sheets to students
+
+##  Requirements
+[NODESJS](https://nodejs.org/en/)
+
+## Working 
+Download **NODEJS** and install.<br>
+In command prompt go to repective directory and clone the repo
+```sh
+git clone https://github.com/indreshp135/AnswerSheetSender
+```
+Then in `AnswerSheetSender` directory there will be `sheets` folder or create it.<br>
+Remove files (if any) and paste all answer sheets (pdf format with first nine letters of file name as full roll no eg:-"`106119052`_ct1 - Indresh.pdf")<br>
+Then fill the required details in .env file(GMAIL, PASSWORD, SUBJECT of mail).<br>
+In command prompt go to AnswerSheetSender directory run 
+```sh
+npm start
+```
+If it throws Auth error, check your mail and allow current activity. (Googles latest update doesn't allow access from new devices) And then give permission for `less secure apps` using [this](https://www.google.com/settings/security/lesssecureapps) link. Then try again 
+```sh
+npm start
+```
+This will log which file is sent to which email address and other errors if any.
---
+++ server.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+const directory = path.join( __dirname,'sheets');
+
+fs.readdir(directory,(err,files)=>{
+    if (err) {
+        console.error("\x1b[31m","Could not list the directory.", err);
+    }
+    if(files){
+        if(files.length===0){
+            console.error("\x1b[31m","No files in sheets directory");
+        }
+        files.forEach(file=>{
+            const name = file;
+            const regno = name.substr(0,9)
+            const format = name.substr(-3,3)
+            if(format==="pdf"){
+                var transporter = nodemailer.createTransport({
+                    service:'gmail',
+                    auth: {
+                        user: process.env.GMAIL,
+                        pass: process.env.PASSWORD
+                    }
+                });
+                const mailOptions = {
+                    from: process.env.GMAIL,
+                    to: regno+'@nitt.edu',
+                    subject: process.env.SUBJECT,
+                    attachments: [
+                        {
+                            filename: file,
+                            path: path.join(__dirname, 'sheets', file),
+                            contentType: 'application/pdf'
+                        }
+                    ]
+                }
+                transporter.sendMail(mailOptions,(err)=>{
+                    if(err)
+                        console.error(err)
+                    else
+                        console.log("\x1b[34m",file,"\x1b[37m",'file sent to',"\x1b[36m", regno +'@nitt.edu' )
+                })
+            }
+            else
+                console.error("\x1b[34m",file,"\x1b[31m",'- Not in pdf format');
+        })
+    }
+    console.log("\x1b[37m")
+})
