cb606d71b51f74a840567dfe43141118b345cb5c
indreshp135 <indreshp135@gmail.com>
2021-05-27 11:48:33 +0530

    Feat: delete files

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This repo can be used to send answer sheets to students
 
 ## Working 
 Download **NODEJS** and install.<br>
-In command prompt go to respective directory and clone the repo
+In command prompt go to respective directory and clone the repo or download zip file and extract
 ```sh
 git clone https://github.com/indreshp135/AnswerSheetSender
 ```
@@ -14,11 +14,20 @@ Then in `AnswerSheetSender` directory there will be `sheets` folder or create it
 Remove files (if any) and paste all answer sheets (pdf format with first nine letters of file name as full roll no eg:-"`106119052`_ct1 - Indresh.pdf")<br>
 Then fill the required details in .env file(GMAIL, PASSWORD, SUBJECT of mail).<br>
 In command prompt go to AnswerSheetSender directory run 
+
 ```sh
 npm start
 ```
+
 If it throws Auth error, check your mail and allow current activity. (Googles latest update doesn't allow access from new devices) And then give permission for `less secure apps` using [this](https://www.google.com/settings/security/lesssecureapps) link. Then try again 
 ```sh
 npm start
 ```
-This will log which file is sent to which email address and other errors if any.
+This will log which file is sent to which email address and other errors if any and delete the pdf after sent.
+
+---
+**NOTE**
+
+The files that got error while sending will remain in sheets directory so we can use ```npm start``` again for sending error files
+
+---
--- server.js
+++ server.js
@@ -1,53 +1,67 @@
-const path = require('path');
-const fs = require('fs');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
-const directory = path.join( __dirname,'sheets');
+const path = require("path");
+const fs = require("fs");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const directory = path.join(__dirname, "sheets");
 
 var transporter = nodemailer.createTransport({
-    service:'gmail',
-    pool:true,
-    auth: {
-        user: process.env.GMAIL,
-        pass: process.env.PASSWORD
-    }
+  service: "gmail",
+  pool: true,
+  auth: {
+    user: process.env.GMAIL,
+    pass: process.env.PASSWORD,
+  },
 });
 
-fs.readdir(directory,(err,files)=>{
-    if (err) {
-        console.error("\x1b[31m","Could not list the directory.", err);
+fs.readdir(directory, (err, files) => {
+  if (err) {
+    console.error("\x1b[31m", "Could not list the directory.", err);
+  }
+  if (files) {
+    if (files.length === 0) {
+      console.error("\x1b[31m", "No files in sheets directory");
     }
-    if(files){
-        if(files.length===0){
-            console.error("\x1b[31m","No files in sheets directory");
-        }
-        files.forEach(file=>{
-            const name = file;
-            const regno = name.substr(0,9)
-            const format = name.substr(-3,3)
-            if(format==="pdf"){
-                const mailOptions = {
-                    from: process.env.GMAIL,
-                    to: regno+'@nitt.edu',
-                    subject: process.env.SUBJECT,
-                    attachments: [
-                        {
-                            filename: file,
-                            path: path.join(__dirname, 'sheets', file),
-                            contentType: 'application/pdf'
-                        }
-                    ]
-                }
-                transporter.sendMail(mailOptions,(err)=>{
-                    if(err)
-                        console.error(err)
-                    else
-                        console.log("\x1b[34m",file,"\x1b[37m",'file sent to',"\x1b[36m", regno +'@nitt.edu' )
-                })
-            }
-            else
-                console.error("\x1b[34m",file,"\x1b[31m",'- Not in pdf format');
-        })
-    }
-    console.log("\x1b[37m")
-})
+    files.forEach((file) => {
+      const name = file;
+      const regno = name.substr(0, 9);
+      const format = name.substr(-3, 3);
+      if (format === "pdf") {
+        const mailOptions = {
+          from: process.env.GMAIL,
+          to: regno + "@nitt.edu",
+          subject: process.env.SUBJECT,
+          attachments: [
+            {
+              filename: file,
+              path: path.join(__dirname, "sheets", file),
+              contentType: "application/pdf",
+            },
+          ],
+        };
+        transporter.sendMail(mailOptions, (err) => {
+          if (err)
+            console.error(
+              "\x1b[34m",
+              file,
+              "\x1b[35m",
+              "file not sent to",
+              "\x1b[36m",
+              regno + "@nitt.edu",
+              err
+            );
+          else
+            console.log(
+              "\x1b[34m",
+              file,
+              "\x1b[37m",
+              "file sent to",
+              "\x1b[36m",
+              regno + "@nitt.edu"
+            );
+          fs.unlinkSync(path.join(__dirname, "sheets", file));
+        });
+      } else console.error("\x1b[34m", file, "\x1b[31m", "- Not in pdf format");
+    });
+  }
+  console.log("\x1b[37m");
+});
